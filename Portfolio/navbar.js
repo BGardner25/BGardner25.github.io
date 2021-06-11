3f2d02b06f595d4c90511e1285ef4fde10cc823a
Bailey <baileygardner25@gmail.com>
2021-06-11 20:35:48 +0100

    added portfolio

---
+++ Portfolio/navbar.js
@@ -0,0 +1,31 @@
+
+// navbar for easy writing into html pages
+document.write("<nav class=\"navbar fixed-top navbar-expand-sm navbar-light shadow-sm mb-5 bg-white rounded\">\n" +
+    "        <div class=\"container\">\n" +
+    "            <a class=\"navbar-brand me-auto\">Bailey Gardner</a>\n" +
+    "            <button class=\"navbar-toggler shadow-none\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarNavAltMarkup\"\n" +
+    "                    aria-controls=\"navbarNavAltMarkup\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
+    "                <span class=\"navbar-toggler-icon\"></span>\n" +
+    "            </button>\n" +
+    "            <div class=\"collapse navbar-collapse\" id=\"navbarNavAltMarkup\">\n" +
+    "                <div class=\"navbar-nav ms-auto\">\n" +
+    "                    <a class=\"nav-link\" href=\"#\">\n" +
+    "                        <span class=\"navbar-custom-collapse\" data-bs-toggle=\"collapse\"\n" +
+    "                              data-bs-target=\".collapse.show\">HOME</span>\n" +
+    "                    </a>\n" +
+    "                    <a class=\"nav-link\" href=\"#about-anchor\">\n" +
+    "                        <span class=\"navbar-custom-collapse\" data-bs-toggle=\"collapse\"\n" +
+    "                              data-bs-target=\".collapse.show\">ABOUT</span>\n" +
+    "                    </a>\n" +
+    "                    <a class=\"nav-link\" href=\"#portfolio-anchor\">\n" +
+    "                        <span class=\"navbar-custom-collapse\" data-bs-toggle=\"collapse\"\n" +
+    "                              data-bs-target=\".collapse.show\">PORTFOLIO</span>\n" +
+    "                    </a>\n" +
+    "                    <a class=\"nav-link\" href=\"#contact-anchor\">\n" +
+    "                        <span class=\"navbar-custom-collapse\" data-bs-toggle=\"collapse\"\n" +
+    "                              data-bs-target=\".collapse.show\">CONTACT</span>\n" +
+    "                    </a>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </nav>");
