e4c3edc8df64e281fee580c3d57dbc24514dde45
Bailey <baileygardner25@gmail.com>
2021-06-12 13:00:50 +0100

    fixed portfolio centering for mobile devices

--- Portfolio/js/navbar.js
+++ Portfolio/js/navbar.js
@@ -1,33 +1,7 @@
-// navbar for easy writing into html pages
-document.write("<nav class=\"navbar fixed-top navbar-expand-sm navbar-light shadow-sm mb-5 bg-white rounded\">\n" +
-    "        <div class=\"container\">\n" +
-    "            <a class=\"navbar-brand me-auto\">Bailey Gardner</a>\n" +
-    "            <button class=\"navbar-toggler shadow-none\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarNavAltMarkup\"\n" +
-    "                    aria-controls=\"navbarNavAltMarkup\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
-    "                <span class=\"navbar-toggler-icon\"></span>\n" +
-    "            </button>\n" +
-    "            <div class=\"collapse navbar-collapse\" id=\"navbarNavAltMarkup\">\n" +
-    "                <div class=\"navbar-nav ms-auto\">\n" +
-    "                    <a class=\"nav-link\" href=\"#home-anchor\">\n" +
-    "                        <span class=\"navbar-custom-collapse\" data-bs-toggle=\"collapse\"\n" +
-    "                              data-bs-target=\".collapse.show\">HOME</span>\n" +
-    "                    </a>\n" +
-    "                    <a class=\"nav-link\" href=\"#about-anchor\">\n" +
-    "                        <span class=\"navbar-custom-collapse\" data-bs-toggle=\"collapse\"\n" +
-    "                              data-bs-target=\".collapse.show\">ABOUT</span>\n" +
-    "                    </a>\n" +
-    "                    <a class=\"nav-link\" href=\"#portfolio-anchor\">\n" +
-    "                        <span class=\"navbar-custom-collapse\" data-bs-toggle=\"collapse\"\n" +
-    "                              data-bs-target=\".collapse.show\">PORTFOLIO</span>\n" +
-    "                    </a>\n" +
-    "                    <a class=\"nav-link\" href=\"#contact-anchor\">\n" +
-    "                        <span class=\"navbar-custom-collapse\" data-bs-toggle=\"collapse\"\n" +
-    "                              data-bs-target=\".collapse.show\">CONTACT</span>\n" +
-    "                    </a>\n" +
-    "                </div>\n" +
-    "            </div>\n" +
-    "        </div>\n" +
-    "    </nav>");
+// write navbar into tag with id=navbar-placeholder
+$(function(){
+    $("#navbar-placeholder").load("html/navbar.html");
+});
 
 // prevent nav bar from updating url when navigating to section of the same page
 $("a.nav-link").on("click", function(event) {
