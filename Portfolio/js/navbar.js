7e53006bca9a005acead7f92dd6edb2c33ea62e5
Bailey <baileygardner25@gmail.com>
2021-06-14 15:35:20 +0100

    removed anchor from url when navigating page

--- Portfolio/js/navbar.js
+++ Portfolio/js/navbar.js
@@ -1,10 +1,4 @@
-// write navbar into tag with id=navbar-placeholder
-$(function(){
-    $("#navbar-placeholder").load("html/navbar.html");
-});
-
-// prevent nav bar from updating url when navigating to section of the same page
-$("a.nav-link").on("click", function(event) {
-   event.preventDefault();
-   $(window).scrollTop($($(this).attr("href")).offset().top);
+$("a.nav-link").click(function() {
+    event.preventDefault();
+    $(window).scrollTop($($(this).attr("href")).offset().top);
 });
